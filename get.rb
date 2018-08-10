b0ca3d9f539713165055188d565056a6d16ffb92
inamuu <kzm0211@gmail.com>
2018-08-10 17:14:29 +0900

    :+1: issueのコメントが取れるところまで

--- get.rb
+++ get.rb
@@ -3,6 +3,14 @@ require 'octokit'
 
 Dotenv.load ".env"
 APIKEY = ENV["APIKEY"]
+REPOS  = ENV["REPOS"]
 
+issues = Octokit.list_issues("#{REPOS}")
 client = Octokit::Client.new(:access_token => "#{APIKEY}")
-puts client.user["login"]
+
+puts issues[0]["title"]
+
+comments = client.issue_comments("#{REPOS}", 2)
+comments.each do |c|
+  puts c.body
+end
